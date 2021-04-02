695d11c06089cfbd2febf5ccb449a44dbc14bf2e
Rafael Cunha <51427048+rocunha09@users.noreply.github.com>
2021-04-02 17:55:14 -0300

    fazendo uso da biblioteca PHPMailer de forma manual realizado require, instanciando o email que será usado pela biblioteca, criado arquivo config.php para realizar configurações do servidor (salvo como config-example), debug setado em 0, aplicação funcionando.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # app-send-mail
 Criando Aplicação Web para envio de E-mails com uso da lib: PHPMailer, e praticando orientação a objetos.
+
+está sendo realizado uso de importação manual da biblioteca para prática, e a versão da biblioteca PHPMailer que 
+está sendo usada é a v6.0.0, que pode ser encontrada aqui: https://github.com/PHPMailer/PHPMailer/tree/v6.0.0 ;
+
+em config-example.php apague a parte "-example" do nome do arquivo, permanecendo apenas: "config.php", 
+então abra e insira o E-mail que será utilizado pela aplicação, com password, e configurações do SMTP do 
+servidor de E-mail que você utiliza.
+
--- processa_envio.php
+++ processa_envio.php
@@ -1,5 +1,18 @@
 <?php
+    //dados de confg do email
+    require_once('config.php');
+    //incluindo classe
     require_once('Mensagem.php');
+    //incluindo lib
+    require "./bibliotecas/PHPMailer/Exception.php";
+    require "./bibliotecas/PHPMailer/OAuth.php";
+    require "./bibliotecas/PHPMailer/PHPMailer.php";
+    require "./bibliotecas/PHPMailer/POP3.php";
+    require "./bibliotecas/PHPMailer/SMTP.php";
+
+    //namespaces
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
 
     $para = NULL;
     $assunto = NULL;
@@ -28,9 +41,48 @@
     echo '</pre>';
 
     $valido = $msg->mensagemValida();
-    if($valido){
-        echo 'mensagem valida';
-    } else {
+    if(!$valido){
         echo 'mensagem invalida';
+        die();
+    }
+
+    //instanciando objeto phpmailer para gerar o email desejado.
+    $mail = new PHPMailer(true);
+    try {
+        //Server settings
+        $mail->SMTPDebug = DEBUG_OUTPUT;                             
+        $mail->isSMTP();                                     
+        $mail->Host = HOST;  
+        $mail->SMTPAuth = ENABLE_AUTH;                        
+        $mail->Username = EMAIL_USERNAME;                
+        $mail->Password = EMAIL_PASSWORD;                          
+        $mail->SMTPSecure =  EMAIL_SECURE;                            
+        $mail->Port = EMAIL_PORT;                                    
+
+        //Recipients
+        $mail->setFrom($mail->Username, 'Remetente');
+        $mail->addAddress($msg->__get('para'), 'Destinatário');     // Add a recipient
+        //$mail->addReplyTo('info@example.com', 'Information');     //encaminha resposta para...
+        //$mail->addCC('cc@example.com');                           //copia
+        //$mail->addBCC('bcc@example.com');                         //copia oculta
+
+        /*
+        //Attachments
+        $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+        $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+        */
+
+        //Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = $msg->__get('assunto');
+        $mail->Body    = $msg->__get('mensagem');;
+        //caso o body acima contenha conteúdo ou tag invalida, o altbody é enviado.
+        //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail->send();
+        echo 'Messagem enviada com sucesso!';
+    } catch (Exception $e) {
+        echo 'Não foi possível enviar este Email, por favor tente novamente mais tarde.';
+        echo '<br>Detalhes do erro: ' . $mail->ErrorInfo;
     }
 ?>
